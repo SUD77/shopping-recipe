909b12966a4c022907dea1ba9e1e18537b09b638
sudhanshujaiswal <jaiswalsudhanshu20@gmail.com>
2023-08-31 10:25:41 +0530

    setup to show recipe items.

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent {
+export class RecipesListComponent implements OnInit{
+
+  // recipes is type of Recipe
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe','This is simply a test','https://pixabay.com/photos/food-meat-recipe-power-pork-1459693/')
+  ];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
